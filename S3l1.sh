ca9060723f174bcc785d5ab3038b188782727f59
Chinmay Joshi <chinmay30joshi@gmail.com>
2026-02-12 21:44:42 +0530

    Add files via upload

---
+++ S3l1.sh
@@ -0,0 +1,172 @@
+#!/bin/bash
+
+# Define color variables
+BLACK_TEXT=$'\033[0;90m'
+RED_TEXT=$'\033[0;91m'
+GREEN_TEXT=$'\033[0;92m'
+YELLOW_TEXT=$'\033[0;93m'
+BLUE_TEXT=$'\033[0;94m'
+MAGENTA_TEXT=$'\033[0;95m'
+CYAN_TEXT=$'\033[0;96m'
+WHITE_TEXT=$'\033[0;97m'
+ORANGE_TEXT=$'\033[38;5;214m'  
+
+NO_COLOR=$'\033[0m'
+RESET_FORMAT=$'\033[0m'
+
+# Define text formatting variables
+BOLD_TEXT=$'\033[1m'
+UNDERLINE_TEXT=$'\033[4m'
+
+
+echo "${ORANGE_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo "${ORANGE_TEXT}${BOLD_TEXT}      SUBSCRIBE TO EASY GAME FOR MORE SUCH TUTORIALS AND GUIDE    ${RESET_FORMAT}"
+echo "${ORANGE_TEXT}${BOLD_TEXT}==================================================================${RESET_FORMAT}"
+echo
+
+# Welcome message with animation
+echo -e "${CYAN}"
+cat << "EOF"
+   _______                _     _   _________
+  |   ___ |              \  \ /  / |   _____ |        _
+  |  |__     __ _   ____  \  V  /  |  |   ___   __ _  \ \ __   __    ____
+  |   __|   / _` | / ___|  \   /   |  |  |   | / _` | | | _  | _  | /  _ \
+  |  |____ | (_| | \ __ \   | |     \  \__|  || (_| | | |  | |  | ||   __/
+  |_______| \__,_| |____/   |_|      \_______| \__,_| |_|  |_|  |_| \____|
+EOF
+echo -e "${NC}"
+
+# Get user input for required values
+echo "${YELLOW_TEXT}Please enter the following configuration details:${RESET}"
+read -p "${YELLOW_TEXT}Enter Zone 1: ${RESET}" ZONE_1
+read -p "${YELLOW_TEXT}Enter Zone 2: ${RESET}" ZONE_2
+read -p "${YELLOW_TEXT}Enter Region: ${RESET}" REGION_1
+read -p "${YELLOW_TEXT}Enter Region: ${RESET}" REGION_2
+
+gcloud compute instances create www-1 \
+    --image-family debian-11 \
+    --image-project debian-cloud \
+    --machine-type e2-micro \
+    --zone ZONE_1 \
+    --tags http-tag \
+    --metadata startup-script="#! /bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      Code"
+
+gcloud compute instances create www-2 \
+    --image-family debian-11 \
+    --image-project debian-cloud \
+    --machine-type e2-micro \
+    --zone ZONE_1 \
+    --tags http-tag \
+    --metadata startup-script="#! /bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      Code"
+
+gcloud compute instances create www-3 \
+    --image-family debian-11 \
+    --image-project debian-cloud \
+    --machine-type e2-micro \
+    --zone ZONE_2 \
+    --tags http-tag \
+    --metadata startup-script="#! /bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      Code"
+
+gcloud compute instances create www-4 \
+    --image-family debian-11 \
+    --image-project debian-cloud \
+    --machine-type e2-micro \
+    --zone ZONE_2 \
+    --tags http-tag \
+    --metadata startup-script="#! /bin/bash
+      apt-get update
+      apt-get install apache2 -y
+      service apache2 restart
+      Code"
+
+gcloud compute firewall-rules create www-firewall \
+    --target-tags http-tag --allow tcp:80
+
+gcloud compute instances list
+
+gcloud compute addresses create lb-ip-cr \
+    --ip-version=IPV4 \
+    --global
+
+read -p "${YELLOW_TEXT}Enter Address: ${RESET}" ADDRESS
+
+gcloud compute instance-groups unmanaged create REGION_1-resources-w --zone ZONE
+
+gcloud compute instance-groups unmanaged create REGION_2-resources-w --zone ZONE 2
+
+gcloud compute instance-groups unmanaged add-instances REGION_1-resources-w \
+    --instances www-1,www-2 \
+    --zone ZONE_1
+
+gcloud compute instance-groups unmanaged add-instances REGION_2-resources-w \
+    --instances www-3,www-4 \
+    --zone ZONE_2
+
+gcloud compute health-checks create http http-basic-check
+
+gcloud compute instance-groups unmanaged set-named-ports REGION_1-resources-w \
+    --named-ports http:80 \
+    --zone ZONE_1
+
+gcloud compute instance-groups unmanaged set-named-ports REGION_2-resources-w \
+    --named-ports http:80 \
+    --zone ZONE_2
+
+gcloud compute backend-services create web-map-backend-service \
+    --protocol HTTP \
+    --health-checks http-basic-check \
+    --global
+
+gcloud compute backend-services add-backend web-map-backend-service \
+    --balancing-mode UTILIZATION \
+    --max-utilization 0.8 \
+    --capacity-scaler 1 \
+    --instance-group REGION-resources-w \
+    --instance-group-zone ZONE \
+    --global
+
+gcloud compute backend-services add-backend web-map-backend-service \
+    --balancing-mode UTILIZATION \
+    --max-utilization 0.8 \
+    --capacity-scaler 1 \
+    --instance-group REGION 2-resources-w \
+    --instance-group-zone ZONE 2 \
+    --global
+
+gcloud compute url-maps create web-map \
+    --default-service web-map-backend-service
+
+gcloud compute target-http-proxies create http-lb-proxy \
+    --url-map web-map
+
+gcloud compute addresses list
+
+
+    gcloud compute forwarding-rules create http-cr-rule \
+    --address ADDRESS \
+    --global \
+    --target-http-proxy http-lb-proxy \
+    --ports 80
+
+
+
+echo
+echo "${ORANGE_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo "${ORANGE_TEXT}${BOLD_TEXT}              LAB COMPLETED SUCCESSFULLY!              ${RESET_FORMAT}"
+echo "${ORANGE_TEXT}${BOLD_TEXT}=======================================================${RESET_FORMAT}"
+echo
+
+echo "${ORANGE_TEXT}${BOLD_TEXT}Subscribe for more: https://www.youtube.com/@Chinmay_Joshi-CJ${RESET_FORMAT}"
+echo "${GREEN_TEXT}${BOLD_TEXT}Don't forget to Like, Share and Subscribe for more Videos${RESET_FORMAT}"
